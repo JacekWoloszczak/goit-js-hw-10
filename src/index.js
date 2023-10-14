1a86ec5fec87a8ae2316d3be9edfe45e088f9969
JacekWoloszczak <jacek.woloszczak@gmail.com>
2023-10-15 00:00:50 +0200

    js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,70 @@
 import SlimSelect from 'slim-select';
 import { fetchBreeds } from './cat-api.js';
 import { fetchCatByBreed } from './cat-api.js';
+
 import Notiflix from 'notiflix';
-import SlimSelect from 'slim-select';
+// import SlimSelect from 'slim-select';
 
 const selectCats = document.querySelector('.breed-select');
-console.log(selectCats);
+// console.log(selectCats);
 const loader = document.querySelector('.loader');
-console.log(loader);
+// console.log(loader);
 const error = document.querySelector('.error');
-console.log(error);
+// console.log(error);
 const catDiv = document.querySelector('.cat-info');
-console.log(catDiv);
+// console.log(catDiv);
 
-selectCats.append(fetchBreeds);
+error.style.opacity = 0;
+loader.style.opacity = 1;
+function getError() {
+  loader.style.opacity = 0;
+  error.style.opacity = 1;
+  console.log(error);
+}
+
+fetchBreeds(selectCats)
+  .then(resolve => {
+    catsLink(resolve);
+  })
+  .catch(error => {
+    return getError;
+  });
+
+const catsLink = catsRules => {
+  catsRules.forEach(i => {
+    const cats = document.createElement('option');
+
+    selectCats.append(cats);
+    cats.textContent = i.name;
+    cats.value = i.id;
+    console.log(cats);
+  });
+};
+const imgCats = showKitty => {
+  fetchCatByBreed(selectCats.value)
+    .then(resolve => {
+      description(resolve);
+    })
+    .catch(error => {
+      return getError;
+    });
+};
+
+const description = img => {
+  fetchBreeds(selectCats.value)
+    .then(resolve => {
+      const check = resolve.find(option => option.id === selectCats.value);
+      catDiv.innerHTML = `
+          <h2>${check.name}</h2>
+          <img src="${img.url}"height="400"></img>
+          <p>${check.description}</p>
+          <p><b>Temperament: </b>${check.temperament}</p>`;
+      catDiv.style.display = 'block';
+      loader.style.display = 'none';
+    })
+    .catch(error => {
+      return console.log(error);
+    });
+};
+
+selectCats.addEventListener('input', imgCats);
